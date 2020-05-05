755c423956d2d33d48b91dcdd72d1f7a4e123284
Aatira B <58064189+aatira1995@users.noreply.github.com>
2020-05-05 11:58:36 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import Routes from './routes';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
+import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
+
+const store = configureStore();
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
+
+render(
+  <Provider store={store}><Routes /></Provider>,
+    document.getElementById('app')
+);
---
+++ src/routes.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Router, browserHistory,  Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import CoursePage from './components/courses/CoursePage';
+import ManageCoursePage from './components/courses/ManageCoursePage';
+
+class Routes extends React.Component {
+    render() {
+        return (
+            <Router history = {browserHistory}>
+            <Route path="/" component={App}>
+              <IndexRoute component={HomePage} />
+              <Route path="/about" component={AboutPage} />
+              <Route path="/courses" component={CoursePage}/>
+              <Route path="/course" component={ManageCoursePage}/>
+              <Route path="/course/:id" component={ManageCoursePage}/>
+            </Route>
+          </Router>
+        );
+    }
+}
+
+export default Routes;
